5b6f1902a371136a3616e48ae7ad673b8a44414c
Kurt Mohler <kurtamohler@gmail.com>
2019-04-24 21:51:13 -0500

    Food min/max input updates
    * Automatically fix input values that do not make sense
    * Add clear buttons
    * Use better vuetify breakpoints to reduce whitespace for larger screens

--- src/components/Food.vue
+++ src/components/Food.vue
@@ -40,55 +40,66 @@
       </v-progress-linear>
     </div>
 
-    <div v-if="showMinMaxSettings || !collapsed">
-      Serving size {{food.serving_amount}} {{food.serving_unit}}
-    </div>
+    <v-layout row wrap align-center>
+      <v-flex
+        xs12
+        sm4
+        v-if="showMinMaxSettings || !collapsed"
+      >
+        Serving size {{food.serving_amount}} {{food.serving_unit}}
+      </v-flex>
 
-    <div
-      v-if="showMinMaxSettings"
-    >
-      <v-layout row align-center>
-        <v-flex
-          xs4
-          sm2
-          class="px-2"
-        >
-          <v-text-field
-            v-model="food.servings_range[0]"
-            type="number"
-            label="min servings"
-            class="noIncButtons"
+      <v-flex
+        xs12
+        sm8
+        v-if="showMinMaxSettings"
+      >
+        <v-layout row align-center>
+          <v-flex
+            xs4
+            class="px-2"
           >
-          </v-text-field>
-        </v-flex>
-        <v-flex
-          xs4
-          sm2
-          class="px-2"
-        >
-          <v-text-field
-            v-model="food.servings_range[1]"
-            :disabled="!food.has_max_servings"
-            :hidden="!food.has_max_servings"
-            type="number"
-            label="max servings"
-            class="noIncButtons"
+            <v-text-field
+              v-model.lazy="food.servings_range[0]"
+              type="number"
+              label="min servings"
+              class="noIncButtons"
+              @blur="verifyMinServings"
+              clearable
+            >
+            </v-text-field>
+          </v-flex>
+          <v-flex
+            xs4
+            class="px-2"
           >
-          </v-text-field>
-        </v-flex>
-        <v-flex
-          xs4
-          class="px-2"
-        >
-          <v-switch
-            label="use max"
-            v-model="food.has_max_servings"
-            height="0"
-            style="transform: scale(0.8); transform-origin: left"
-          ></v-switch>
-        </v-flex>
-      </v-layout>
-    </div>
+            <v-text-field
+              v-model.lazy="food.servings_range[1]"
+              :disabled="!food.has_max_servings"
+              :hidden="!food.has_max_servings"
+              type="number"
+              label="max servings"
+              class="noIncButtons"
+              @blur="verifyMaxServings"
+              clearable
+            >
+            </v-text-field>
+          </v-flex>
+          <v-flex
+            xs4
+            class="px-2"
+          >
+            <v-switch
+              label="use max"
+              v-model="food.has_max_servings"
+              height="0"
+              @change="verifyMaxServings"
+              style="transform: scale(0.8); transform-origin: left"
+            ></v-switch>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
 
     <div
       :hidden="collapsed"
@@ -145,7 +156,37 @@
         } else {
           this.collapsed = true
         }
+      },
 
+      // Ensure min servings is positive and not greater than
+      // the max servings. Change it if needed.
+      verifyMinServings: function(event) {
+        let min = parseFloat(this.food.servings_range[0])
+        let max = parseFloat(this.food.servings_range[1])
+
+        if (isNaN(min) || min < 0) {
+          this.$set(
+            this.food.servings_range, 0, 0
+          )
+
+        } else if (this.food.has_max_servings && (min > max)) {
+          this.$set(
+            this.food.servings_range, 0, max
+          )
+        }
+      },
+
+      // Ensure max servings is positive and not less than
+      // the min servings. Change it if needed.
+      verifyMaxServings: function(event) {
+        let min = parseFloat(this.food.servings_range[0])
+        let max = parseFloat(this.food.servings_range[1])
+
+        if (this.food.has_max_servings && (isNaN(max) || max < min)) {
+          this.$set(
+            this.food.servings_range, 1, min
+          )
+        }
       }
     }
   }
