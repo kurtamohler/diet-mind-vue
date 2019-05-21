529751178c9134b3a9d23a5e86e904a39ee183cc
Kurt Mohler <kurtamohler@gmail.com>
2019-05-20 19:57:20 -0500

    Added pie charts for nutrition contribution tables

--- src/components/NutritionDisplayRow.vue
+++ src/components/NutritionDisplayRow.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    @click="show_contrib = !show_contrib"
-    v-ripple
   >
     <v-layout
+      @click="show_contrib = !show_contrib"
+      v-ripple
       row
       wrap
       align-center
@@ -31,8 +31,26 @@
       class="mx-4 mb-3"
     >
       <div class="light-blue darken-2 px-2 py-1 white--text">
-        <h4>Food Contributions</h4>
+        <h4>Foods containing this nutrient</h4>
       </div>
+      <div
+        class="pa-2"
+        v-if="nutrient.value == 0"
+      >
+        None
+      </div>
+
+      <div
+        class="py-2"
+        v-if="nutrient.value > 0"
+      >
+        <pie-chart
+          :data="contrib_chart_data"
+          :library="contrib_chart_options"
+        >
+        </pie-chart>
+      </div>
+
       <div
         v-for="(food, ind) in nutrient.food_contrib"
         :key="ind"
@@ -44,19 +62,22 @@
           class="pa-2"
         >
           <v-flex xs9 class="pr-2">
-            {{food.name}}
+            {{food.name}} ({{food.weight}})
           </v-flex>
 
           <v-flex xs3 class="pl-1">
             {{food.percentage}} %
           </v-flex>
         </v-layout>
+
       </div>
+
     </v-card>
   </div>
 </template>
 
 <script>
+
 export default {
   props: [
     'nutrient',
@@ -65,9 +86,64 @@ export default {
 
   data: function () {
     return {
-      'show_contrib': false
+      'show_contrib': false,
+      'contrib_chart_data': null,
+      'contrib_chart_options': null
     }
   },
+
+  mounted() {
+    if (this.nutrient && this.nutrient.food_contrib) {
+      this.fillContribChartData()
+      this.fillContribChartOptions()
+    }
+  },
+
+  methods: {
+    limitStrLen(str, l=20){
+      if (str.length > l) {
+        return str.substring(0, l) + '...'
+      } else {
+        return str
+      }
+    },
+    fillContribChartData() {
+      let chart_data = []
+
+      let str_limit = 100
+
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+          str_limit = 20
+          break;
+        case 'sm':
+          str_limit = 60
+          break;
+      }
+
+      for (let ind in this.nutrient.food_contrib) {
+        let food = this.nutrient.food_contrib[ind]
+
+        chart_data.push([
+          this.limitStrLen(food.name, str_limit)+ ' (' + food.weight + ')',
+          food.percentage
+        ])
+      }
+
+      this.contrib_chart_data = chart_data
+
+
+    },
+    fillContribChartOptions() {
+      let chart_options = {
+        legend: {
+          display: false
+        }
+      }
+
+      this.contrib_chart_options = chart_options
+    }
+  }
 } 
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import App from './App.vue'
 
 import VueLodash from 'vue-lodash'
 import '@mdi/font/css/materialdesignicons.css'
+
+import VueChartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+
 // import VueScrollTo from 'vue-scrollto'
 let VueScrollTo = require('vue-scrollto')
 
@@ -14,6 +18,8 @@ Vue.config.productionTip = false
 Vue.use(VueLodash, {name: 'lodash'})
 Vue.use(Vuetify, {iconfont: 'mdi'})
 Vue.use(VueScrollTo)
+
+Vue.use(VueChartkick, {adapter: Chart})
 
 new Vue({
   render: h => h(App),
